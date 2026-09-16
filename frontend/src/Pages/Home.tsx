f063c23b381a987ac5c3d4b8fc94d79730d9613d
bharath <buddy.rohith@gmail.com>
2026-09-17 03:57:36 +0530

    career changes

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,4 +1,4 @@
-import { ArrowRight, ArrowUpRight, CheckCircle2 } from 'lucide-react';
+import { ArrowRight, ArrowUpRight } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 
